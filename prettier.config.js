867ebb9fe37ffb049beccfcb4eb10c12f996773b
shun916 <118881278+code-polaris044@users.noreply.github.com>
2024-01-15 22:20:58 +0900

    github actions

--- prettier.config.js
+++ prettier.config.js
@@ -1,9 +1,10 @@
 /** @type {import("pretter").Config} */
 
 const config = {
-    semi: true,
-    tabWidth: 4,
-    singleQuote: true,
+	semi: true,
+	tabWidth: 4,
+	singleQuote: true,
+	useTabs: true,
 };
 
 export default config;
